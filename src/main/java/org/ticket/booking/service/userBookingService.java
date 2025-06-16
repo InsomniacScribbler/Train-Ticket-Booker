8a273d4399a7156da7ef866a378736e261ff5dec
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-06-16 06:35:51 +0530

    Getter Setter

--- src/main/java/org/ticket/booking/service/userBookingService.java
+++ src/main/java/org/ticket/booking/service/userBookingService.java
@@ -10,13 +10,21 @@ import java.util.List;
 
 public class userBookingService {
     private User user;
+
     private List<User> userList;
+
     private static  final ObjectMapper objectMapper = new ObjectMapper();
+
     private static final String USERS_PATH ="../localDb/users.json";
+
     private static final String TRAINS_PATH ="../localDb/trains.json";
+
     public userBookingService(User user) throws IOException {
+
         this.user =  user;
+
         File users = new File(USERS_PATH);
+
         userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
 
     }
